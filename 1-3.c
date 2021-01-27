1c7d39223aa17710918669bc40b7d93d7b6e4b28
Pacc <66855833+Pacc0@users.noreply.github.com>
2021-01-27 11:35:41 -0600

    Exercises 1.3-1.5

---
+++ 1-3.c
@@ -0,0 +1,5 @@
+#include "stdio.h"
+int main(void) {
+  printf("Hello, this is a form feed using \\f.\f\n");
+  printf("As you can see, there was a line of white space for forms, hence the name.\n");
+}
